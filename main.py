6ebdebfcac2fae78c5f30b4cd46dbb7ba36f9521
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-07-23 15:45:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+st.header("English")
+if st.button("Brazilian Portuguese"):
+    st.switch_page("pages/main_br.py")
+
+#title
+st.title("🧮 Multifunctional Calculator")
+st.subheader("Choose the type of calculation you want")
+
+if "type_calc" not in st.session_state:
+    st.session_state.type_calc = None
+
+#sidebar
+st.sidebar.write("Type of calculation:")
+
+if st.sidebar.button("Golden Material"):
+    st.session_state.type_calc = "Golden Material"
+if st.sidebar.button("Basic Math"):
+    st.session_state.type_calc = "Basic Math"
+if st.sidebar.button("Scientific"):
+    st.session_state.type_calc = "Scientific"
+if st.sidebar.button("Medication Dosis"):
+    st.session_state.type_calc = "Medication Dosis"
+if st.sidebar.button("Finantial"):
+    st.session_state.type_calc = "Finantial"
+
+type_calc = st.session_state.type_calc
+
+#contas básicas
+if st.session_state.type_calc == "Basic Math":
+    st.markdown("### 🔢 Basic Opertation")
+
+    num_a = st.number_input("Type first number: ", value=0.0)
+    num_b = st.number_input("Type second number: ", value=0.0)
+
+    operation_input = st.selectbox(
+        "Choose the operation:",
+        [
+            "Select...",
+            "+ (Addition)",
+            "- (Subtraction)",
+            "* (Multiply)",
+            "/ (Divide)",
+        ],
+    )
+
+    if st.button("Calculate"):
+        if "+ (Addition)" in operation_input:
+            st.success(f"Result: {num_a + num_b}")
+
+        elif "- (Subtraction)" in operation_input:
+            st.success(f"Result: {num_a - num_b}")
+
+        elif "* (Multiply)" in operation_input:
+            st.success(f"Result: {num_a * num_b}")
+
+        elif "/ (Divide)" in operation_input:
+            if num_b != 0:
+                st.success(f"Resultado: {num_a / num_b}")
+            else:
+                st.error("Error: Divide by zero!")
+
+        else:
+            st.warning("Please, select a valid operation.")
+
+elif type_calc in ["Scientific", "Golden Material", "Medication Dosis", "Finantial"]:
+    st.info(f"scient, golden, dosis, fin in development.")
---
+++ pages/main_br.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+st.header("Português Brasileiro")
+if st.button("Inglês"):
+    st.switch_page("main.py")
+
+#title
+st.title("🧮 Calculadora Multifuncional")
+st.subheader("Escolha o tipo de cálculo que você quer fazer")
+
+if "type_calc" not in st.session_state:
+    st.session_state.type_calc = None
+
+#sidebar
+st.sidebar.write("Tipo de cálculo:")
+
+if st.sidebar.button("Material Dourado"):
+    st.session_state.type_calc = "Material Dourado"
+if st.sidebar.button("Básica"):
+    st.session_state.type_calc = "Básica"
+if st.sidebar.button("Científica"):
+    st.session_state.type_calc = "Científica"
+if st.sidebar.button("Dose de Medicação"):
+    st.session_state.type_calc = "Dose de Medicação"
+if st.sidebar.button("Financeiro"):
+    st.session_state.type_calc = "Financeiro"
+
+type_calc = st.session_state.type_calc
+
+#contas básicas
+if st.session_state.type_calc == "Básica":
+    st.markdown("### 🔢 Operações Básicas")
+
+    num_a = st.number_input("Digite o primeiro número: ", value=0.0)
+    num_b = st.number_input("Digite o segundo número: ", value=0.0)
+
+    operation_input = st.selectbox(
+        "Escolhe a operação:",
+        [
+            "Selecione...",
+            "+ (Soma)",
+            "- (Subtração)",
+            "* (Multiplicação)",
+            "/ (Divisão)",
+        ],
+    )
+
+    if st.button("Calcular"):
+        if "+ (Soma)" in operation_input:
+            st.success(f"Resultado: {num_a + num_b}")
+
+        elif "- (Subtração)" in operation_input:
+            st.success(f"Resultado: {num_a - num_b}")
+
+        elif "* (Multiplicação)" in operation_input:
+            st.success(f"Resultado: {num_a * num_b}")
+
+        elif "/ (Divisão)" in operation_input:
+            if num_b != 0:
+                st.success(f"Resultado: {num_a / num_b}")
+            else:
+                st.error("Erro: Divisão por zero!")
+
+        else:
+            st.warning("Por favor, selecione uma operação válida.")
+
+elif type_calc in ["cientifica", "material dourado", "dose de medicao", "rendimentos"]:
+    st.info(f"A seção de cientifica, material dourado, dose de medicao, rendimentos está em desenvolvimento.")

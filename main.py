49b085f8846eb512383571843b06ba6440121016
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-07-28 11:48:46 -0300

    added financial and bottom

--- main.py
+++ main.py
@@ -163,4 +163,98 @@ elif st.session_state.type_calc == "Medication Dosis":
 elif st.session_state.type_calc == "Financial":
     st.markdown("### 🪙 Financial")
 
-    st.warning("Ongoing")
+    operation_input = st.selectbox(
+        "Choose the financial planning:",
+        [
+            "Select...",
+            "Monthly Expenses",
+            "Financial Runaway",
+            "Creating Reserve Funding",            
+        ],
+    )
+
+    if "Monthly Expenses" in operation_input:
+        st.write("You can type your total income and total expense or type each expense")
+        income = st.number_input("Type your monthly income: ", value=0.0)
+        total_expense = st.number_input("Type your total expense: ", value=0.0)
+
+        if total_expense == 0.0:
+            rent = st.number_input("Rent/Housing: ", value=0.0)
+            water = st.number_input("Water bill: ", value=0.0)
+            power = st.number_input("Power bill: ", value=0.0)
+            phone = st.number_input("Phone/Internet bill: ", value=0.0)
+            school = st.number_input("School/Education costs: ", value=0.0)
+            food = st.number_input("Supermarket: ", value=0.0)
+            transport = st.number_input("Gasoline/Transport: ", value=0.0)
+            credit_card = st.number_input("Credit Card: ", value=0.0)
+            loan = st.number_input("Loans: ", value=0.0)
+            other = st.number_input("Others: ", value=0.0)
+
+            total_expense_calc = rent+water+power+phone+school+food+transport+credit_card+loan+other
+            st.success(f"Total expenses: {total_expense_calc}")
+
+            st.success(f"Balance: {income - total_expense_calc}")
+
+        else:
+            st.success(f"Balance: {income - total_expense}")
+
+    elif "Financial Runaway" in operation_input:
+        savings = st.number_input("Type your total savings: ", value=0.0)
+        monthly_yield = st.number_input("Type approximate monthly yield in % (0,5 suggested for saving account): ", value=0.5)
+        withdraw = st.number_input("Type your montly withdraw: ", value=0.0)
+        deposit = st.number_input("Type your montly deposit: ", value=0.0)
+
+        if st.button("Duration of the savings in months:"):
+
+            if deposit >= withdraw:
+                st.warning("Your savings will last forever.")  
+
+            else:
+                duration = 0
+
+                savings_update = withdraw - deposit
+        
+                while savings >= savings_update:
+                    savings = savings - savings_update
+                    savings = savings + savings * monthly_yield / 100
+                    duration = duration + 1
+
+                st.success(duration)
+
+    elif "Creating Reserve Funding" in operation_input:
+        goal = st.number_input("Type your savings goal: ", value=0.0)
+        monthly_yield = st.number_input("Type approximate monthly yield in % (0,5 suggested for saving account): ", value=0.5)
+        deposit = st.number_input("Type your montly deposit: ", value=0.0)
+
+        if st.button("Calculate the time you need to reach your goal:"):
+
+            if deposit <= 0.0:
+                st.warning("Without a monthly deposit you will never reach your goal.")  
+
+            else:
+                month = 0
+                reserve = 0
+                     
+                while reserve < goal:
+                    reserve = reserve + deposit
+                    reserve = reserve + reserve * monthly_yield / 100
+                    month = month + 1
+
+                st.success(f"Months to reach your goal: {month}")
+                goal_years = round(month / 12)
+                st.success(f"Years to reach your goal: {goal_years}")
+
+    else:
+        st.warning("Please, select a financial planning.")
+
+#bottom
+with st.bottom:
+
+    st.write("Personal Project to practice Python by Vitor A Pantoja")
+    link1, link2, link3 = st.columns(3)
+    with link1:
+        st.link_button("E-mail", "vitorpantoja.dev@gmail.com")
+    with link2:
+        st.link_button("Github", "https://github.com/VitorPantojaDev")
+    with link3:
+        st.link_button("Linkedin", "https://www.linkedin.com/in/vitorapantoja/")
--- pages/main_br.py
+++ pages/main_br.py
@@ -163,6 +163,98 @@ elif st.session_state.type_calc == "Dose de Medicação":
 elif st.session_state.type_calc == "Financeiro":
     st.markdown("### 🪙 Financeiro")
 
-    st.warning("Em andamento")
+    operation_input = st.selectbox(
+        "Escolha o planejamento financeiro:",
+        [
+            "Selecione...",
+            "Gastos Mensais",
+            "Fundo de emergência",
+            "Criando fundo de reserva",            
+        ],
+    )
 
+    if "Gastos Mensais" in operation_input:
+        st.write("Você pode digitar seu ganho mensal total e seu gasto mensal total ou digitar cada uma das suas despesas")
+        income = st.number_input("Digite seu ganho mensal total: ", value=0.0)
+        total_expense = st.number_input("Digite seu gasto mensal total: ", value=0.0)
 
+        if total_expense == 0.0:
+            rent = st.number_input("Aluguel/Moradia: ", value=0.0)
+            water = st.number_input("Conta de água: ", value=0.0)
+            power = st.number_input("Conta de luz: ", value=0.0)
+            phone = st.number_input("Telefone/Internet: ", value=0.0)
+            school = st.number_input("Escola/Custo com educação: ", value=0.0)
+            food = st.number_input("Supermercado: ", value=0.0)
+            transport = st.number_input("Gasolina/Transporte: ", value=0.0)
+            credit_card = st.number_input("Cartão de Crédito: ", value=0.0)
+            loan = st.number_input("Emprestimo: ", value=0.0)
+            other = st.number_input("Outros: ", value=0.0)
+
+            total_expense_calc = rent+water+power+phone+school+food+transport+credit_card+loan+other
+            st.success(f"Despesas totais: {total_expense_calc}")
+
+            st.success(f"Balanço: {income - total_expense_calc}")
+
+        else:
+            st.success(f"Balanço: {income - total_expense}")
+
+    elif "Fundo de emergência" in operation_input:
+        savings = st.number_input("Digite seu valor guardado: ", value=0.0)
+        monthly_yield = st.number_input("Digite o seu rendimento mensal em % (0,5 sugerido para poupança): ", value=0.5)
+        withdraw = st.number_input("Digite seu saque mensal: ", value=0.0)
+        deposit = st.number_input("Digite seu depósito mensal: ", value=0.0)
+
+        if st.button("Duração do seu valor guardado em meses:"):
+
+            if deposit >= withdraw:
+                st.warning("Seu valor guardado vai durar para sempre.")  
+
+            else:
+                duration = 0
+
+                savings_update = withdraw - deposit
+        
+                while savings >= savings_update:
+                    savings = savings - savings_update
+                    savings = savings + savings * monthly_yield / 100
+                    duration = duration + 1
+
+                st.success(duration)
+
+    elif "Criando fundo de reserva" in operation_input:
+        goal = st.number_input("Digite seu objetivo: ", value=0.0)
+        monthly_yield = st.number_input("Digite o seu rendimento mensal em % (0,5 sugerido para poupança): ", value=0.5)
+        deposit = st.number_input("Digite seu depósito mensal: ", value=0.0)
+
+        if st.button("Calcule o tempo que precisa para alcançar seu objetivo:"):
+
+            if deposit <= 0.0:
+                st.warning("Sem um depósito mensal você nunca vai alcançar seu objetivo.")  
+
+            else:
+                month = 0
+                reserve = 0
+                     
+                while reserve < goal:
+                    reserve = reserve + deposit
+                    reserve = reserve + reserve * monthly_yield / 100
+                    month = month + 1
+
+                st.success(f"Meses para alcançar seu objetivo: {month}")
+                goal_years = round(month / 12)
+                st.success(f"Anos para alcançar seu objetivo: {goal_years}")
+
+    else:
+        st.warning("Por favor, selecione o seu planejamento fincaneiro.")  
+
+#bottom
+with st.bottom:
+
+    st.write("Projeto pessoal para praticar Python por Vitor A Pantoja")
+    link1, link2, link3 = st.columns(3)
+    with link1:
+        st.link_button("E-mail", "vitorpantoja.dev@gmail.com")
+    with link2:
+        st.link_button("Github", "https://github.com/VitorPantojaDev")
+    with link3:
+        st.link_button("Linkedin", "https://www.linkedin.com/in/vitorapantoja/")
